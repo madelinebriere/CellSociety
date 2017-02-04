107df4329aadbb10ed30de7010a29ab0aaa3e266
Stone Mathers <smm117@duke.edu>
2017-02-04 17:33:24 -0500

    Created PopUp

--- src/file_handling/PopUp.java
+++ src/file_handling/PopUp.java
@@ -1,7 +1,28 @@
 package file_handling;
 
-public class PopUp {
 
 
+import java.io.File;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+public class PopUp{
+
+	public Stage myStage;
+	
+	public PopUp(Stage stage){
+		myStage = stage;
+	}
+	
+	public SimulationType getSimulation(){
+		XMLParser parser = new XMLParser();
+		return parser.getSimulation(getFile());	
+	}
+
+	private File getFile(){
+		FileChooser chooser = new FileChooser();
+		chooser.setTitle("Select XML File");
+		return chooser.showOpenDialog(myStage);
+	}
 	
 }
