b2e772e60f481c08466e5edf3ef0b54bad1a4cc6
Talha Koc <talha.koc@duke.edu>
2017-02-10 16:15:59 -0500

    maddies changes

--- src/societal_level/CellSociety.java
+++ src/societal_level/CellSociety.java
@@ -2,7 +2,7 @@ package societal_level;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -11,12 +11,8 @@ import cellular_level.*;
 import file_handling.*;
 import data_structures.*;
 import javafx.scene.paint.Color;
-import util.BorderChooser;
-import util.NeighborsChooser;
-import neighbors.*;
-import sim_rules.*;
-import borders.*;
-
+import util.CellData;
+import util.Location;
 
 /**
  * 1) Figure out empty color situation
@@ -42,42 +38,45 @@ import borders.*;
  *         the superclass
  */
 
-public abstract class CellSociety {
+public class CellSociety {
 	private static final Color DEFAULT_COLOR = Color.WHITE;
 	
 	private CellShape myShape;
 	private Dimensions mySize;
-	private Map<CellName, List<Cell>> currentCells;
+	private Collection<Cell> currentCells;
 	private Color emptyColor;
-	private Neighbors neighbors;
-	private Border border;
-	private CellName defaultCell;
-	private SimRules sim;
 	
 	/**
 	 * Default 
 	 */
-
 	public CellSociety(){
 		this(generateDefaultData());
 	}
+	
+	private static SimulationData generateDefaultData(){
+		HashMap<CellName, CellRatio> m = new HashMap<CellName, CellRatio>();
+		m.put(CellName.FISH_CELL, new CellRatio(0.5));
+		m.put(CellName.SHARK_CELL, new CellRatio(0.2));
+		m.put(CellName.EMPTY_CELL, new CellRatio(0.3));
+		CellRatioMap r = new CellRatioMap(m);
+		SimulationData d = new SimulationData(
+				new Dimensions(10,10), SimulationName.WATER_SOCIETY, r, CellShape.SQUARE, true, Color.WHITE 
+				);
+		return d;
+	}
+	
+	
 	public CellSociety(SimulationData sim) {
-		setMyShape(sim.getShape());
-		setSize(sim.getDimensions());
-		setBorder(BorderChooser.chooseBorder(sim));
-		setNeighbors(NeighborsChooser.chooseNeighbors(border, sim.getShape()));
-		setSim(sim.getSim());
-		makeCells(sim);
+		myShape = sim.getShape();
+		mySize = sim.getDimensions();
+		CellRatioMap myRatio = sim.getRatio();
+		makeCells(myRatio);
 	}
 	
 	public CellSociety(SimulationType sim) {
 		setCurrentCells(sim.getCells());
 		setSize(new Dimensions(sim.getDimension(), sim.getDimension()));
-		setSim(sim.getSimRules());
-		
 	}
-	
-	
 	
 
 	/**
@@ -88,7 +87,7 @@ public abstract class CellSociety {
 	 */
 	public Color[][] getCurrentColors() {
 		Color[][] toRet = new Color[getX()][getY()];
-		/**Color emptyColor = getEmptyColor();
+		Color emptyColor = getEmptyColor();
 		for (int i = 0; i < getX(); i++) {
 			for (int j = 0; j < getY(); j++) {
 				toRet[i][j] = emptyColor;
@@ -107,16 +106,14 @@ public abstract class CellSociety {
 				toRet[c.getMyRow()][c.getMyCol()] = setColor;
 
 			}
-		}**/
+		}
 		return toRet;
-		//TODO: Fix this to account for non-positive indices
 	}
 
 	
 
 	/**
-	 *	Step function (update) -- Applies the current rules of the simulation
-	 * To each of the current Cells. Sorts the current cells by Cell-defined preference
+	 *	Step function (update) -- Sorts the current cells by Cell-defined preference
 	 * (decided with the compareTo method in each Cell), putting certain types
 	 * of Cells first on the list the update. This is used in WaterWorld to have
 	 * the Sharks update before the fish so that the eaten fish can be removed
@@ -127,50 +124,37 @@ public abstract class CellSociety {
 	 * @return 2D Array of current Cell colors
 	 */
 	public Color[][] step() {
-		applyCurrentRules();
 		sortByPriorityCurrentCells();
 		shuffleCurrentCells();
 		stepAllCells(new ArrayList<EmptyCell>(getAllEmptyCells()));
-		applyCurrentColors();
 		return getCurrentColors();
 	}
 
 	
-	/**
-	 * Assumptions made here:
-	 * 	-> Correct cell types have been given 
-	 * 	-> Dimensions are even (no incomplete rows)
-	 * 
-	 * @param sim
-	 * @return
-	 */
-	public abstract List<Cell> makeCells(SimulationData sim);
-	
-	/**
-	 * Called during each update to apply the most recent colors for the society
-	 */
-	public abstract void applyCurrentColors();
+	public Collection<Cell> makeCells(CellRatioMap r){
+		ArrayList<Cell> newCells = new ArrayList<Cell>();
+		for(CellName n: r.getMapOfCellsRatios().keySet()){
+			Cell newCell = CellGenerator.newCell(n);
+		}
+		
+		return newCells;
+	}
 
-	/**
-	 * Called during each update to apply to most recent rules to all of the cells
-	 */
-	public abstract void applyCurrentRules();
-	
-	/**
+	/** 
 	 * EmptyCells getter
 	 * 
-	 * @return List of all emptyCells in the currentCells
+	 * @return Collection of all emptyCells in the currentCells
 	 */
-	protected List<EmptyCell> getAllEmptyCells() {
+	protected Collection<EmptyCell> getAllEmptyCells() {
 		return getEmptyCells(getCurrentCells());
 	}
 
 	/**
 	 * TARGETED EmptyCells getter
 	 * 
-	 * @return List of all emptyCells in the given List
+	 * @return Collection of all emptyCells in the given Collection
 	 */
-	public List<EmptyCell> getEmptyCells(List<Cell> possibleOptions) {
+	public Collection<EmptyCell> getEmptyCells(Collection<Cell> possibleOptions) {
 		ArrayList<EmptyCell> toRet = new ArrayList<EmptyCell>();
 		for (Cell c : possibleOptions) {
 			if (c instanceof EmptyCell) {
@@ -213,29 +197,28 @@ public abstract class CellSociety {
 	}
 
 	/**
-	 * ABSTRACT BECAUSE EACH SUBCLASS MUST DEFINE THE 
-	 * 
 	 * Updates the cell by passing it a new CellData object with defined
 	 * available spots and knowledge of this specific Cell Society
 	 * 
 	 * @param c
 	 *            Cell for update
 	 * @param available
-	 *            List of available cells
+	 *            Collection of available cells
 	 * @return All updated cells from cell updates (new baby cells, moved cells,
 	 *         etc.)
 	 */
-	private List<Cell> updateCell(Cell c, List<EmptyCell> available){
+	private Collection<Cell> updateCell(Cell c, Collection<EmptyCell> available) {
 		return c.update(new CellData(this, available));
 	}
+
 	/**
-	 * Iterate through given List and fill any cell-less locations in a
+	 * Iterate through given Collection and fill any cell-less locations in a
 	 * size x size grid with a new EmptyCell
 	 * 
 	 * @param nextGen
-	 *            The List of cells to be padded
+	 *            The Collection of cells to be padded
 	 */
-	public void fillEmptySpots(List<Cell> nextGen) {
+	public void fillEmptySpots(Collection<Cell> nextGen) {
 		int[][] filled = new int[getX()][getY()];
 		for (Cell c : nextGen) {
 			filled[c.getMyRow()][c.getMyCol()] += 1;
@@ -288,22 +271,241 @@ public abstract class CellSociety {
 	 *            Cell whose neighbors are returned
 	 * @return neighbors of Cell c
 	 */
-	public List<Cell> neighbors(Cell c){
+	public Collection<Cell> neighbors(Cell c){
+		return getNeighbors(c);
+	}
+
+	/**
+	 * Normal neighbors function, gets any adjacent cells
+	 * 
+	 * @param c
+	 *            Cell of interest
+	 * @return Neighbors of Cell c
+	 */
+	protected Collection<Cell> getNeighbors(Cell c) {
 		ArrayList<Cell> neighbors = new ArrayList<Cell>();
-		//return getNeighbors(c);
+		for (Cell possible : currentCells) {
+			if (isAdjacent(c, possible)) {
+				neighbors.add(possible);
+			}
+		}
 		return neighbors;
 	}
-	
-	private static SimulationData generateDefaultData(){
-		HashMap<CellName, CellRatio> m = new HashMap<CellName, CellRatio>();
-		m.put(CellName.FISH_CELL, new CellRatio(0.5));
-		m.put(CellName.SHARK_CELL, new CellRatio(0.2));
-		m.put(CellName.EMPTY_CELL, new CellRatio(0.3));
-		CellRatioMap r = new CellRatioMap(m);
-		SocietyData s = new SocietyData(false, Color.WHITE, r, CellName.EMPTY_CELL);
-		BoardData b = new BoardData ();
-		SimulationData d = new SimulationData(SimulationName.WATER_SOCIETY, b, s, new WaterSimRules());
-		return d;
+
+	/**
+	 * Wrapped neighbors function, gets any adjacent cells, INCLUDING those
+	 * cells that may be accessible across the board (moving from the far left
+	 * to the far right, or the top to the bottom)
+	 * 
+	 * @param c
+	 *            Cell of interest
+	 * @return Wrapped neighbors of Cell c
+	 */
+	protected Collection<Cell> getWrappedNeighbors(Cell c) {
+		ArrayList<Cell> neighbors = new ArrayList<Cell>();
+		for (Cell possible : currentCells) {
+			if (isAnyAdjacent(c, possible)) {
+				neighbors.add(possible);
+			}
+		}
+		return neighbors;
+	}
+
+	/**
+	 * Limited neighbors function, gets any adjacent cells IN THE CARDINAL
+	 * DIRECTIONS (N, S, E, W)
+	 * 
+	 * @param c
+	 *            Cell of interest
+	 * @return Cardinal neighbors of Cell c
+	 */
+	protected Collection<Cell> getCardinalNeighbors(Cell c) {
+		ArrayList<Cell> neighbors = new ArrayList<Cell>();
+		for (Cell possible : currentCells) {
+			if (isCardinalAdjacent(c, possible)) {
+				neighbors.add(possible);
+			}
+		}
+		return neighbors;
+	}
+
+	/**
+	 * Limited neighbors function, gets any adjacent cells IN THE CARDINAL
+	 * DIRECTIONS (N, S, E, W), includes movement across the board
+	 * 
+	 * @param c
+	 *            Cell of interest
+	 * @return Cardinal neighbors of Cell c
+	 */
+	protected Collection<Cell> getWrappedCardinalNeighbors(Cell c) {
+		ArrayList<Cell> neighbors = new ArrayList<Cell>();
+		for (Cell possible : currentCells) {
+			if (isWrappedCardinalAdjacent(c, possible)) {
+				neighbors.add(possible);
+			}
+		}
+		return neighbors;
+	}
+
+	/**
+	 * Adjacency in common sense
+	 * 
+	 * @param c1
+	 *            target cell 1
+	 * @param c2
+	 *            target cell 2 (for comparison)
+	 * @return true if adjacent, false otherwise
+	 */
+	public boolean isAdjacent(Cell c1, Cell c2) {
+		return isAdjacent(c1.getMyLocation(), c2.getMyLocation());
+	}
+
+	protected boolean isAdjacent(Location l1, Location l2) {
+		return isCardinalAdjacent(l1, l2) || (oneAwayVertical(l1, l2) && oneAwayHorizontal(l1, l2));
+	}
+
+	/**
+	 * Adjacency, but only in cardinal directions (N, S, E, W)
+	 * 
+	 * @param c1
+	 *            target cell 1
+	 * @param c2
+	 *            target cell 2 (for comparison)
+	 * @return true if cardinal adjacent, false otherwise
+	 */
+	public boolean isCardinalAdjacent(Cell c1, Cell c2) {
+		return isCardinalAdjacent(c1.getMyLocation(), c2.getMyLocation());
+	}
+
+	public boolean isCardinalAdjacent(Location l1, Location l2) {
+		return (sameColumn(l1, l2) && oneAwayVertical(l1, l2)) || (sameRow(l1, l2) && oneAwayHorizontal(l1, l2));
+	}
+
+	/**
+	 * Adjacency EVEN ACROSS BOARD (wrapped sides)
+	 * 
+	 * @param c1
+	 *            target cell
+	 * @param c2
+	 *            target cell for comparison
+	 * @return true if wrapped adjacent, false otherwise
+	 */
+	protected boolean isWrappedAdjacent(Cell c1, Cell c2) {
+		return isWrappedAdjacent(c1.getMyLocation(), c2.getMyLocation());
+	}
+
+	protected boolean isWrappedAdjacent(Location l1, Location l2) {
+		return (sameColumn(l1, l2) && inRowAcrossBoard(l1, l2)) || (sameRow(l1, l2) && inColAcrossBoard(l1, l2));
+	}
+
+	/**
+	 * Adjacency EVEN ACROSS BOARD (wrapped sides), includes cardinal cells
+	 * 
+	 * @param c1
+	 *            target cell
+	 * @param c2
+	 *            target cell for comparison
+	 * @return true if wrapped adjacent, false otherwise
+	 */
+	protected boolean isWrappedCardinalAdjacent(Cell c1, Cell c2) {
+		return isWrappedCardinalAdjacent(c1.getMyLocation(), c2.getMyLocation());
+	}
+
+	protected boolean isWrappedCardinalAdjacent(Location l1, Location l2) {
+		return isWrappedAdjacent(l1, l2) || isCardinalAdjacent(l1, l2);
+	}
+
+	/**
+	 * Includes both common adjacency and wrapped adjacency
+	 * 
+	 * @param c1
+	 *            Target cell 1
+	 * @param c2
+	 *            Target cell 2
+	 * @return true if any type of adjacent, false otherwise
+	 */
+	public boolean isAnyAdjacent(Cell c1, Cell c2) {
+		return isAnyAdjacent(c1.getMyLocation(), c2.getMyLocation());
+	}
+
+	protected boolean isAnyAdjacent(Location l1, Location l2) {
+		return isWrappedAdjacent(l1, l2) || isAdjacent(l1, l2);
+	}
+
+	/**
+	 * Check for adjacency by column ACROSS the board
+	 * 
+	 * @param l1
+	 *            location 1
+	 * @param l2
+	 *            location 2 for comparison
+	 * @return true if l1 and l2 are in adjacent columns, only considering
+	 *         wrapped motion (e.g., if one cell is in the farthest left column
+	 *         and the other is in the farthest right, this method will return
+	 *         TRUE).
+	 */
+	private boolean inColAcrossBoard(Location l1, Location l2) {
+		return (l1.getMyCol() == 0 && l2.getMyCol() == getX() - 1) || (l2.getMyCol() == 0 && l1.getMyCol() == getX() - 1);
+	}
+
+	/**
+	 * Check for adjacency by row ACROSS the board
+	 * 
+	 * @param l1
+	 *            location 1
+	 * @param l2
+	 *            location 2 for comparison
+	 * @return true if l1 and l2 are in adjacent row, only considering wrapped
+	 *         motion (e.g., if one cell is in the top row and the other is in
+	 *         the lowest row, this method will return TRUE).
+	 */
+	private boolean inRowAcrossBoard(Location l1, Location l2) {
+		return (l1.getMyRow() == 0 && l2.getMyRow() == getY() - 1) || (l2.getMyRow() == 0 && l1.getMyRow() == getY() - 1);
+	}
+
+	/**
+	 * Check for same column/row
+	 */
+
+	private boolean sameColumn(Location l1, Location l2) {
+		return l2.getMyCol() == l1.getMyCol();
+	}
+
+	private boolean sameRow(Location l1, Location l2) {
+		return l2.getMyRow() == l1.getMyRow();
+	}
+
+	/**
+	 * Check distance in directions
+	 */
+
+	private boolean oneAwayVertical(Location l1, Location l2) {
+		return oneAwayDown(l1, l2) || oneAwayUp(l1, l2);
+	}
+
+	private boolean oneAwayHorizontal(Location l1, Location l2) {
+		return oneAwayRight(l1, l2) || oneAwayLeft(l1, l2);
+	}
+
+	/**
+	 * Check if l1 and l2 are one away in each direction (up, down, right,
+	 * left), return true if this is the case, false otherwise
+	 */
+
+	private boolean oneAwayUp(Location l1, Location l2) {
+		return ((l1.getMyRow() - 1) == l2.getMyRow());
+	}
+
+	private boolean oneAwayDown(Location l1, Location l2) {
+		return ((l1.getMyRow() + 1) == l2.getMyRow());
+	}
+
+	private boolean oneAwayRight(Location l1, Location l2) {
+		return ((l1.getMyCol() + 1) == l2.getMyCol());
+	}
+
+	private boolean oneAwayLeft(Location l1, Location l2) {
+		return ((l1.getMyCol() - 1) == l2.getMyCol());
 	}
 
 	/**
@@ -326,11 +528,11 @@ public abstract class CellSociety {
 		setCurrentCells(orderedCells);
 	}
 
-	public List<Cell> getCurrentCells() {
+	public Collection<Cell> getCurrentCells() {
 		return currentCells;
 	}
 
-	public void setCurrentCells(List<Cell> current) {
+	public void setCurrentCells(Collection<Cell> current) {
 		currentCells = current;
 	}
 
@@ -361,47 +563,10 @@ public abstract class CellSociety {
 	public CellShape getMyShape() {
 		return myShape;
 	}
-	
 
 	public void setMyShape(CellShape myShape) {
 		this.myShape = myShape;
 	}
-
-	public Neighbors getNeighbors() {
-		return neighbors;
-	}
-
-	public void setNeighbors(Neighbors neighbors) {
-		this.neighbors = neighbors;
-	}
-
-	public Border getBorder() {
-		return border;
-	}
-
-	public void setBorder(Border border) {
-		this.border = border;
-	}
-
-	public CellName getDefaultCell() {
-		return defaultCell;
-	}
-
-	public void setDefaultCell(CellName defaultCell) {
-		this.defaultCell = defaultCell;
-	}
-
-	public SimRules getSim() {
-		return sim;
-	}
-
-	public void setSim(SimRules sim) {
-		this.sim = sim;
-	}
-	
-	
-	
-	
 	
 	
 }
