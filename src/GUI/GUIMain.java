824181fc29c67d6dddbe1aa02fa4b8acd46f6ac9
Talha Koc <talha.koc@duke.edu>
2017-02-12 01:54:06 -0500

    committing fixed gui stuff

--- src/GUI/GUIMain.java
+++ src/GUI/GUIMain.java
@@ -5,7 +5,6 @@ package GUI;
 import java.util.HashMap;
 import java.util.Random;
 
-import cellular_level.RawData;
 import data_structures.BoardData;
 import data_structures.BorderType;
 import data_structures.CellShape;
